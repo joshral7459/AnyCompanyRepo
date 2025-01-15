626f2b97f80fb8558f41fb11fc719a6ae70df389
joshral7459 <joshua7459@gmail.com>
2025-01-15 09:42:04 -0600

    Update quote.php

--- html/quote.php
+++ html/quote.php
@@ -117,7 +117,7 @@ function getTaskArn() {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <title>AnyCompany Insurance Quote Tool</title>
+    <title>AnyCompany Insurance Quote Tool - BAD CODE</title>
     <style>
         /* Base styles */
         * {
@@ -368,7 +368,7 @@ function getTaskArn() {
     
     <header>
         <div class="header-content">
-            <h1>AnyCompany Insurance Quote Tool</h1>
+            <h1>AnyCompany Insurance Quote Tool - BAD CODE</h1>
             <div class="user-info">
                 Welcome, Employee | ID: EMP123 | <a href="#" class="logout-link">Logout</a>
             </div>
